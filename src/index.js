9f8dd9df2e16cff5bf303df44ef34fdbbae69db4
Kevin Peña <kevin.pena.prog@gmail.com>
2017-09-21 22:35:30 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './polyfill'
+// Main entry point. You can use latest ES features
+// There are ES7 polyfills and also util.promisify
+// Make sure to run yarn and then yarn dev to get started
